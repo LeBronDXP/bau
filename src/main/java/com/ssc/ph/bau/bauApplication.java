cfd0eb6ed20f7bd7df58be3c42b98ef9188d33e7
LeBron <wangbq714@outlook.com>
2019-08-19 21:00:35 +0800

    add files

---
+++ src/main/java/com/ssc/ph/bau/bauApplication.java
@@ -0,0 +1,17 @@
+package com.ssc.ph.bau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class bauApplication 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(bauApplication.class, args);
+    }
+}
---
+++ src/main/java/com/ssc/ph/bau/controller/BauApplicationController.java
@@ -0,0 +1,16 @@
+package com.ssc.ph.bau.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+
+public class BauApplicationController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
